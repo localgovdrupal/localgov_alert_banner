4f76d8fc8ca9e31f61e522bade17bda2f2219fb5
Andy Broomfield <andy.broomfield@brighton-hove.gov.uk>
2020-06-23 12:11:34 +0100

    fix missing docblock comments on AlertBannerState

--- src/AlertBannerState.php
+++ src/AlertBannerState.php
@@ -3,38 +3,49 @@
 namespace Drupal\localgov_alert_banner;
 
 use Drupal\Core\Entity\ContentEntityBase;
-use Drupal\Core\State\State;
+use Drupal\Core\State\StateInterface;
 
 /**
  * Class AlertBannerState.
  *
  * @package Drupal\localgov_alert_banner
+ * @ingroup localgov_alert_banner
  */
 class AlertBannerState {
 
   /**
-   * @var \Drupal\Core\State\State
+   * State object.
+   *
+   * @var \Drupal\Core\State\StateInterface
    */
   protected $state;
 
   /**
-   * @var array
+   * Alert banner hide token.
+   *
+   * @var string
    */
   protected $token;
 
   /**
    * AlertBannerState constructor.
    *
-   * @param Drupal\Core\State\State $state
+   * @param Drupal\Core\State\StateInterface $state
+   *   State service.
    */
-  public function __construct(State $state) {
+  public function __construct(StateInterface $state) {
     $this->state = $state;
     $this->token = $state->get('localgov_alert_banner.alert_banner_token');
   }
 
   /**
-   * @param  \Drupal\Core\Entity\ContentEntityBase $entity
+   * Generate token.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityBase $entity
+   *   Alert banner entity.
+   *
    * @return $this
+   *   Self (class can be be method chained).
    */
   public function generateToken(ContentEntityBase $entity) {
     $prefix = 'alert-' . $entity->id();
@@ -45,14 +56,20 @@ class AlertBannerState {
   }
 
   /**
-   * @return string
+   * Get token.
+   *
+   * @return mixed
+   *   The token, or NULL if not set.
    */
   public function getToken() {
     return $this->token ?? NULL;
   }
 
   /**
+   * Save token.
+   *
    * @return mixed
+   *   Result from saving to state.
    */
   public function save() {
     return $this->state->set('localgov_alert_banner.alert_banner_token', $this->token);
