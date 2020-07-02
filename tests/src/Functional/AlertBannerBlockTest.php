37f0819f5725af47ba1d14bbc0d186760ba56673
Dan Champion <support@championinternet.com>
2020-07-02 10:56:07 +0100

    Remove hide link test

--- tests/src/Functional/AlertBannerBlockTest.php
+++ tests/src/Functional/AlertBannerBlockTest.php
@@ -108,4 +108,37 @@ class AlertBannerBlockTest extends BrowserTestBase {
     $this->assertSession()->pageTextNotContains($title);
   }
 
+  /**
+   * Test remove hide link option.
+   */
+  public function testAlertRemoveHideLink() {
+    $title = $this->randomMachineName(8);
+    $alert_message = 'Alert message: ' . $this->randomMachineName(16);
+    $alert = $this->container->get('entity_type.manager')->getStorage('localgov_alert_banner')
+      ->create([
+        'type' => 'localgov_alert_banner',
+        'title' => $title,
+        'short_description' => $alert_message,
+        'type_of_alert' => 'minor',
+        'remove_hide_link' => 0
+      ]);
+    $alert->save();
+
+    // Flag the alert banner to put it live.
+    $flag_service = $this->container->get('flag');
+    $flag = $flag_service->getFlagById('localgov_put_live');
+    $flag_service->flag($flag, $alert);
+
+    // Check hide link is shown when remove_hide_link is not set
+    $this->drupalGet('<front>');
+    $this->assertSession()->responseContains('js-alert-banner-close');
+    $this->assertSession()->pageTextContains('Hide');
+
+    // Check title is not shown when remove_hide_link is set
+    $alert->set('remove_hide_link', ['value' => 1]);
+    $alert->save();
+    $this->drupalGet('<front>');
+    $this->assertSession()->responseNotContains('js-alert-banner-close');
+  }
+
 }
