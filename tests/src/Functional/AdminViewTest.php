e477fce7674fdf21b67c7128871d978c208fa09b
Andy Broomfield <andybroomfield@gmail.com>
2020-07-21 23:33:37 +0100

    Add basic test for admin view loaded
    Test that the admin view loads
    Test the entity collection goes to same url

---
+++ tests/src/Functional/AdminViewTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\Tests\localgov_alert_banner\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+use Symfony\Component\HttpFoundation\Response;
+use Drupal\Core\Url;
+
+/**
+ * Functional tests for LocalGovDrupal Alert banner admin view.
+ */
+class AdminViewTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'claro';
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'localgov_alert_banner',
+  ];
+
+  /**
+   * Test load the alert banner admin view.
+   */
+  public function testLoadAdminView() {
+    // Create an admin user -- @todo move to set up.
+    $adminUser = $this->createUser([], 'admintestuser', TRUE);
+    $this->drupalLogin($adminUser);
+
+    // Check can access the admin view dashboard.
+    $this->drupalGet('admin/content/alert-banner');
+    $this->assertResponse(Response::HTTP_OK);
+
+    // Check this is the view by making sure certian view only text is present.
+    // @todo Work out how to make sure this is the view path (Kernal test?).
+    $this->assertText('Manage Alert Banners');
+
+    // Check that loading the collection URL loads the admin dashboard.
+    $collectionUrl = Url::fromRoute('entity.localgov_alert_banner.collection');
+    $this->drupalGet($collectionUrl);
+    $this->assertUrl('admin/content/alert-banner');
+  }
+
+}
