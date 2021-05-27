087637890509ee460b8ee3cb6d0d9094e8409710
Adnan <adnan.muhammad@croydon.gov.uk>
2021-05-27 20:01:38 +0100

    Full page alert banner submodule.

---
+++ modules/localgov_alert_banner_full_page/js/full-page-alert-banner.es6.js
@@ -0,0 +1,55 @@
+/**
+ * @file
+ * Present a full page alert banner as a Bootstrap modal.
+ *
+ * The alert banner is added by the localgov_alert_banner module.
+ *
+ * @see localgov_alert_banner_preprocess_localgov_alert_banner__full()
+ * @see localgov-alert-banner--full.html.twig
+ */
+
+(function launchModalAlertBanner(jQuery, Drupal, drupalSettings, bootstrap) {
+  Drupal.behaviors.launchModalAlertBanner = {
+    attach() {
+      const modalId =
+        drupalSettings.localgov_alert_banner_full_page.localgov_full_page_alert_banner_id;
+
+      const modal = jQuery(`#${modalId}`).get(0);
+      if (typeof modal === "undefined") {
+        return;
+      }
+
+      // Display this modal only when the Alert banner module has not hidden it.
+      if (this.isHiddenAlert(modal)) {
+        return;
+      }
+
+      const bsModal = new bootstrap.Modal(modal);
+      bsModal.show();
+
+      // Attach modal closer as a click event handler of the "Hide" link.
+      jQuery(".js-localgov-alert-banner__close", modal).click(() => bsModal.hide());
+    },
+
+    /**
+     * Is this a hidden alert?
+     *
+     * @param {object} modal
+     *   jQuery object.
+     *
+     * @return {bool}
+     *   Is the given alert hidden?
+     *
+     * @see localgov_alert_banner/js/alert_banner.js
+     */
+    isHiddenAlert(modal) {
+      const cookie = jQuery.cookie("hide-alert-banner-token");
+      const cookieTokens =
+        typeof cookie !== "undefined" ? cookie.split("+") : [];
+
+      const dismissToken = jQuery(modal).data("dismiss-alert-token");
+      const isHidden = cookieTokens.includes(dismissToken);
+      return isHidden;
+    }
+  };
+})(jQuery, Drupal, drupalSettings, bootstrap); // eslint-disable-line
