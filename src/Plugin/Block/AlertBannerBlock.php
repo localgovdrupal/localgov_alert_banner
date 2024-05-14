d2df4247cd1a672eda29d5792e313cf8a31173e3
Andy Broomfield <andy.broomfield@brighton-hove.gov.uk>
2024-05-14 13:02:48 +0100

    Replace getTranslation with entityRespositry::getTranslationFromContext
    Use the entitiy repositry service to get translation in correct language
    with fallback.

--- src/Plugin/Block/AlertBannerBlock.php
+++ src/Plugin/Block/AlertBannerBlock.php
@@ -4,9 +4,9 @@ namespace Drupal\localgov_alert_banner\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\Cache;
+use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\field\Entity\FieldStorageConfig;
@@ -38,11 +38,11 @@ class AlertBannerBlock extends BlockBase implements ContainerFactoryPluginInterf
   protected $currentUser;
 
   /**
-   * The language manager service.
+   * The entity repository services.
    *
-   * @var \Drupal\Core\Language\LanguageManagerInterface
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
    */
-  protected $languageManager;
+  protected $entityRepository;
 
   /**
    * Constructs a new AlertBannerBlock.
@@ -57,14 +57,14 @@ class AlertBannerBlock extends BlockBase implements ContainerFactoryPluginInterf
    *   The entity type manager service.
    * @param \Drupal\Core\Session\AccountProxyInterface $current_user
    *   Current user service.
-   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
-   *   The language manager service.
+   * @param Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository service.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, AccountProxyInterface $current_user, LanguageManagerInterface $language_manager) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, AccountProxyInterface $current_user, EntityRepositoryInterface $entity_repository) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $entity_type_manager;
     $this->currentUser = $current_user;
-    $this->languageManager = $language_manager;
+    $this->entityRepository = $entity_repository;
   }
 
   /**
@@ -88,7 +88,7 @@ class AlertBannerBlock extends BlockBase implements ContainerFactoryPluginInterf
       $plugin_definition,
       $container->get('entity_type.manager'),
       $container->get('current_user'),
-      $container->get('language_manager'),
+      $container->get('entity.repository'),
     );
   }
 
@@ -201,14 +201,9 @@ class AlertBannerBlock extends BlockBase implements ContainerFactoryPluginInterf
 
     // Load alert banners and add all.
     // Visibility check happens in build, so we get cache contexts on all.
-    $langcode = $this->languageManager->getCurrentLanguage()->getId();
     foreach ($published_alert_banners as $alert_banner_id) {
       $alert_banner = $this->entityTypeManager->getStorage('localgov_alert_banner')->load($alert_banner_id);
-
-      // Get alert banner translation if it exists.
-      if ($alert_banner->hasTranslation($langcode)) {
-        $alert_banner = $alert_banner->getTranslation($langcode);
-      }
+      $alert_banner = $this->entityRepository->getTranslationFromContext($alert_banner);
 
       $is_accessible = $alert_banner->access('view', $this->currentUser);
       if ($is_accessible) {
