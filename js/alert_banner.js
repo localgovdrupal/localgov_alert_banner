f501f8d2a25260cc1af265e20e564eb47fb3cc8e
Andy Broomfield <andy.broomfield@brighton-hove.gov.uk>
2024-12-19 12:37:25 +0000

    Remove DomcontentLoaded event
    Since javascript is already in Drupal behaviours attach function, the dom
    should already be present. Once behaviours should prevent multiple attachments
    
    Moved the setAlertBannerHideCookie function outside the attach.

--- js/alert_banner.js
+++ js/alert_banner.js
@@ -5,72 +5,69 @@
  * This is so if the alert changes, the banner is reshown.
  */
 
+function setAlertBannerHideCookie(cookieTokens, token) {
+  cookieTokens.push(token);
+  const newCookie = cookieTokens.join("+");
+  // Set expiry 30 days.
+  const expiry = Date.now() + 30 * 24 * 60 * 60 * 1000;
+  document.cookie = `hide-alert-banner-token=${newCookie}; expires=${new Date(
+    expiry
+  ).toUTCString()}; SameSite=Lax;`;
+}
+
 (function localgovAlertBannerScript(Drupal) {
   Drupal.behaviors.localgovAlertBanners = {
     attach(context) {
-      "use strict";
 
-      function setAlertBannerHideCookie(cookieTokens, token) {
-        cookieTokens.push(token);
-        const newCookie = cookieTokens.join("+");
-        // Set expiry 30 days.
-        const expiry = Date.now() + 30 * 24 * 60 * 60 * 1000;
-        document.cookie = `hide-alert-banner-token=${newCookie}; expires=${new Date(
-          expiry
-        ).toUTCString()}; SameSite=Lax;`;
+      const allCookies = document.cookie.split("; ");
+      let cookie;
+
+      for (let i = 0; i < allCookies.length; i++) {
+        const indvCookie = allCookies[i].split("=");
+        if (indvCookie[0] === "hide-alert-banner-token") {
+          cookie = indvCookie[1];
+        }
       }
 
-      document.addEventListener("DOMContentLoaded", function () {
-        const allCookies = document.cookie.split("; ");
-        let cookie;
+      const cookieTokens =
+        typeof cookie !== "undefined" ? cookie.split("+") : [];
 
-        for (let i = 0; i < allCookies.length; i++) {
-          const indvCookie = allCookies[i].split("=");
-          if (indvCookie[0] === "hide-alert-banner-token") {
-            cookie = indvCookie[1];
+      const alertBanners = once(
+        "allAlertBanners",
+        ".js-localgov-alert-banner",
+        context
+      );
+
+      if (alertBanners) {
+        alertBanners.forEach(function (banner) {
+          banner.classList.remove("hidden");
+          const token = banner.getAttribute("data-dismiss-alert-token");
+          if (cookieTokens.includes(token)) {
+            banner.style.display = "none";
           }
-        }
+        });
+      }
 
-        const cookieTokens =
-          typeof cookie !== "undefined" ? cookie.split("+") : [];
+      const alertBannerCloseButtons = once(
+        "allAlertBannerCloseButtons",
+        ".js-localgov-alert-banner__close",
+        context
+      );
 
-        const alertBanners = once(
-          "allAlertBanners",
-          ".js-localgov-alert-banner",
-          context
-        );
-
-        if (alertBanners) {
-          alertBanners.forEach(function (banner) {
-            banner.classList.remove("hidden");
-            const token = banner.getAttribute("data-dismiss-alert-token");
-            if (cookieTokens.includes(token)) {
-              banner.style.display = "none";
-            }
+      if (alertBannerCloseButtons) {
+        alertBannerCloseButtons.forEach(function (closeButton) {
+          closeButton.addEventListener("click", function (e) {
+            e.preventDefault();
+            const banner = closeButton.closest(".js-localgov-alert-banner");
+            banner.setAttribute("aria-hidden", "true");
+            banner.style.display = "none";
+            setAlertBannerHideCookie(
+              cookieTokens,
+              banner.getAttribute("data-dismiss-alert-token")
+            );
           });
-        }
-
-        const alertBannerCloseButtons = once(
-          "allAlertBannerCloseButtons",
-          ".js-localgov-alert-banner__close",
-          context
-        );
-
-        if (alertBannerCloseButtons) {
-          alertBannerCloseButtons.forEach(function (closeButton) {
-            closeButton.addEventListener("click", function (e) {
-              e.preventDefault();
-              const banner = closeButton.closest(".js-localgov-alert-banner");
-              banner.setAttribute("aria-hidden", "true");
-              banner.style.display = "none";
-              setAlertBannerHideCookie(
-                cookieTokens,
-                banner.getAttribute("data-dismiss-alert-token")
-              );
-            });
-          });
-        }
-      });
+        });
+      }
     },
   };
 })(Drupal);
